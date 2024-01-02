24e39463c62ec85bef3fa627613ddd08003d24f7
Ignas Mikenas <mikenasignas@gmail.com>
2024-01-02 15:56:28 +0200

    CollocationCardTitle, updated to show  companies with matching racks in premises

--- src/components/DLCJournalComponents/VisitiRegistrationComponents/CollocationCardTitle.tsx
+++ src/components/DLCJournalComponents/VisitiRegistrationComponents/CollocationCardTitle.tsx
@@ -1,42 +1,50 @@
 /* eslint-disable max-len */
-import React                                                from 'react'
+import React from 'react'
 import { CollocationsType, CompaniesType, CompanyInfoType } from '../../../types/globalTypes'
-import { get }                                              from '../../../Plugins/helpers'
-import { useCookies }                                       from 'react-cookie'
-import { Tag }                                              from 'antd'
+import { get } from '../../../Plugins/helpers'
+import { useCookies } from 'react-cookie'
+import { Tag } from 'antd'
 
 type DLCEmployeeEscortTagProps = {
-    category:   string;
-    values:     string[]
-    index:      number
-}
+  category: string;
+  values: string[];
+  index: number;
+};
 
 type MatchingCompaniesType = {
-    companyName:  string;
-    premiseName:  string;
-    racks:        string[]
-  }
+  companyName: string;
+  premiseName: string;
+  racks: string[];
+  site: string;
+};
 
-const CollocationCardTitle = ({category, values, index}: DLCEmployeeEscortTagProps) => {
-  const [cookies]                               = useCookies(['access_token'])
-  const [allCollocations, setAllCollocations]   = React.useState<CollocationsType[]>()
-  const [allCompanies, setAllCompanies]         = React.useState<CompaniesType[]>()
+const CollocationCardTitle = ({ category, values, index }: DLCEmployeeEscortTagProps) => {
+  const [cookies] = useCookies(['access_token'])
+  const [allCollocations, setAllCollocations] = React.useState<CollocationsType[]>()
+  const [allCompanies, setAllCompanies] = React.useState<CompaniesType[]>()
+
   React.useEffect(() => {
     (async () => {
-      const companies     = await get('getCompanies', cookies.access_token)
-      const collocations  = await get('getCollocations', cookies.access_token)
+      const companies = await get('getCompanies', cookies.access_token)
+      const collocations = await get('getCollocations', cookies.access_token)
       setAllCollocations(collocations.data[0].colocations)
       setAllCompanies(companies.data)
     })()
   }, [])
-  const getMatchingCompanies = (allCollocations: CollocationsType[] | undefined, companyInfo: CompanyInfoType) => {
+
+  const getMatchingCompanies = (
+    allCollocations: CollocationsType[] | undefined,
+    companyInfo: CompanyInfoType
+  ) => {
     const matchingCompanies: MatchingCompaniesType[] = []
     Object.keys(companyInfo).forEach((site) => {
       const siteCollocations = allCollocations?.find((collocation) => collocation.site === site)
       if (siteCollocations) {
         const companyColl = companyInfo[site]
         companyColl?.forEach((company) => {
-          const premises = siteCollocations.premises.find((el) => Object.keys(company)[0] === el.premiseName)
+          const premises = siteCollocations.premises.find(
+            (el) => Object.keys(company)[0] === el.premiseName
+          )
           if (premises) {
             const racks = company[Object.keys(company)[0]]
             const matchingRacks = premises.racks.filter((rack) => racks.includes(rack))
@@ -45,6 +53,7 @@ const CollocationCardTitle = ({category, values, index}: DLCEmployeeEscortTagPro
                 companyName: companyInfo.companyName,
                 premiseName: premises.premiseName,
                 racks:       matchingRacks,
+                site:        site,
               })
             }
           }
@@ -54,28 +63,31 @@ const CollocationCardTitle = ({category, values, index}: DLCEmployeeEscortTagPro
     return matchingCompanies
   }
 
-  const companyCollocation = allCompanies?.map((el) => getMatchingCompanies(allCollocations, el.companyInfo)).flat().flat()
-  const companiesWithMatchingRacks = new Set()
-  let hasMatchingRack = false
-  companyCollocation?.forEach((match, j) => {
-    if (index !== j) {
-      const doCompaniesHaveMatchingRacks = match.racks.some(rack => values.includes(rack)) &&
-      match.premiseName === category
+  const companyCollocation = allCompanies
+    ?.map((el) => getMatchingCompanies(allCollocations, el.companyInfo))
+    .flat()
 
-      if (doCompaniesHaveMatchingRacks) {
-        companiesWithMatchingRacks.add(match.companyName)
-        hasMatchingRack = true
-      }
+
+  const companiesHasMatchingRacks  = companyCollocation?.map((match, i) => {
+    if (
+      index !== i &&
+        match.racks.some((rack) => values.includes(rack)) &&
+        match.premiseName === category
+    ){
+      return match
     }
   })
+
+  const companiesWithMatchingRacks = companiesHasMatchingRacks?.filter((el) => el !== undefined)
   return (
-    <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>{category}</div>
-      {hasMatchingRack && (
-        <Tag key={`${category}-tag`} color='blue'>
-            Reikalinga DLC inžinieriaus palyda
+      {
+        companiesWithMatchingRacks && companiesWithMatchingRacks?.length > 1 &&
+        <Tag color='blue'>
+              Reikalinga DLC inžinieriaus palyda
         </Tag>
-      )}
+      }
     </div>
   )
 }
